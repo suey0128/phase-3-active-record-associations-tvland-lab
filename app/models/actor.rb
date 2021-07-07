c90f9380c15f452d6424675360c2f4873e35879c
suey0128 <syui0128@gmail.com>
2021-07-07 12:24:30 -0700

    commit tree d7f197682d0f8fb78d1e6cc19ded7ad7401865fa

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        #find the first name and last name and group them into a new string
+        self.first_name + ' ' + self.last_name
+    end
+
+    def list_roles
+        #return an array of string, char_name - show_name
+        self.characters.map{|character| character.name + " - " + character.show.name}
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map{|character| character.actor.first_name + " " + character.actor.last_name}
+    end
+
 end
